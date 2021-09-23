bb530cf3c755cb223d03b04ea9b2e50ac772634c
Guigas <61693858+AlvarezGui@users.noreply.github.com>
2021-09-23 08:43:59 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,13 +3,33 @@ var server = require('./config/server');
 var app = server.app;
 var porta = server.porta;
 
+//configurações do banco de dadps
+var mongoose = require('mongoose');
+var conexao = ()=>{
+    var caminho = mongoose.connect('mongodb+srv://awesome:180904@cluster0.fdcse.mongodb.net/mongoatlas?retryWrites=true&w=majority');
+}
+var schema = mongoose.Schema;
+var depoimentos = new schema({
+    nome:String,
+    cargo:String,
+    mensagem:String
+});
+var documentos = mongoose.model('depoimentos', depoimentos);
+
 //setar ejs como engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 //setar rota index.ejs
 app.get('/',(req, res)=>{
-    res.render('index');
+    conexao();
+    documentos.find().limit(3).sort({'_id': -1})
+    .then((documentos)=>{
+        res.render('index', {documentos});
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
 });
 
 
